b10f5ba7f2339f3c5b548d251640f291482e5cb5
Ridhi-16 <ridhi8012@gmail.com>
2025-06-12 14:10:30 +0530

    save

---
+++ JS/varEx.js
@@ -0,0 +1,30 @@
+var num=Math.random()
+num+=2
+console.log(num);
+ var num1=Math.round(num);
+console.log("num after round",num1);
+ var num2=Math.ceil(num);
+console.log("num after ceil",num2);
+ var num3=Math.floor(num);
+console.log("num after floor",num3);
+var name="ridhi"
+console.log(name.length);
+console.log(name.charAt(3));
+printName("ridhi")
+printName("siya")
+function printName(x){
+    console.log("my name is ",x);   
+}
+
+
+setInterval(() => {
+    console.log("hi ")
+}, 2000);
+
+var sum=(a,b)=>{
+    
+}
+
+
+
+
